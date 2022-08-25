d3553a8f4156c5d204ac9440960fd4e3f8ce1d0b
seif <baazouziseif9@gmail.com>
2022-08-25 09:29:26 +0100

    get logs every 2h

--- run.sh
+++ run.sh
@@ -7,4 +7,12 @@ export ESHOP_HOSTNAME="172.17.0.1"
 
 export DB_HOSTNAME="172.17.0.4"
 
-python -B get-logs.py
+(
+  while true; do
+    python -B get-logs.py
+    sleep $(( 60*60*2 )) # 2h 
+
+  done
+) &
+
+wait
