34a6f2a8fddad6bea9d78dd712cade287e080f2e
seif <baazouziseif9@gmail.com>
2022-08-25 14:29:37 +0100

    show today data

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from datetime import date
+from flask import Flask, render_template
 
-app = Flask(__name__)
+from db.mongo import db
+
+app = Flask(__name__, static_url_path="/public", static_folder="/home/seif/projects/e-shop/seo-dashboard/public")
 
 @app.get("/")
 def index():
-  return "<h1>Hello, World!</h1>"
+  today = date.today().strftime("%Y-%m-%d")
+  log = db.logs.find_one({ "date": today })
+
+  return render_template("index.html", log=log, fields=[ "users", "shops", "items", "carts", "shopsRates", "itemsRates" ])
 
 if __name__ == "__main__":
   app.run(debug=True, port=8080, host="0.0.0.0")
