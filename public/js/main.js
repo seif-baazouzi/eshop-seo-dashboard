cccff6b7a05a6ee021f5b8001033c367d424b0ab
seif <baazouziseif9@gmail.com>
2022-08-27 14:02:34 +0100

    add number animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+// chart
 const myChart = echarts.init(document.getElementById("main"))
 myChart.setOption(options)
 window.onresize = () => myChart.resize()
+
+// numbers animation
+
+const numbers = document.querySelectorAll("[number-anim]")
+
+numbers.forEach(num => {
+  const number = parseInt(num.getAttribute("number-anim"))
+  for (let value = 0; value <= number; value++) {
+    setTimeout(() => num.innerText = value, 10*value)
+  }
+})
